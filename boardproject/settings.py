fd2babad13e2e25bf264dd2c37b6cce18224c06f
shingo kubo <62503741+k-shingo@users.noreply.github.com>
2020-09-28 00:41:27 +0900

    Update settings.py

--- boardproject/settings.py
+++ boardproject/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%@h^26t$csfm2rprxh+9tm34f9x5pleje1*v%hkzu=gw^&kxwo'
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
